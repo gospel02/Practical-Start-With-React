1957c2cba35d03d15d2cda9e1f4a0ee5c0ea81aa
Casey Randall <42552122+gospel02@users.noreply.github.com>
2019-04-07 20:04:03 -0600

    initial commit

---
+++ src/main-page/index.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import AppPresentation from './app-presentation';
+import logo from './logo.svg';
+import './main-page.css';
+
+
+class App extends Component {
+
+  state = {}
+
+  componentDidMount(){
+    this.fetchHouses();
+  }
+
+  fetchHouses = () => {
+    fetch('/houses.json')
+    .then(rsp => rsp.json())
+    .then(allHouses => {
+      this.allHouses = allHouses;
+      this.determineFeaturedHouse();
+      this.determineUniqueCountries();
+    })
+  }
+
+  determineFeaturedHouse = () => {
+    if (this.allHouses) {
+      const randomIndex = Math.floor(Math.random() * this.allHouses.length);
+      const featuredHouse = this.allHouses[randomIndex];
+      this.setState({ featuredHouse });
+    };
+  }
+
+  determineUniqueCountries = () => {
+    const countries = this.allHouses
+    ? Array.from(new Set(this.allHouses.map(h => h.country)))
+    : [];
+    countries.unshift(null);
+    this.setState({ countries });
+  }
+
+  filterHouses = (country) => {
+    this.setState({activeHouse: null });
+    const filteredHouses = this.allHouses.filter((h) => h.country === country);
+    this.setState({ filteredHouses });
+    this.setState({ country });
+  }
+
+  setActiveHouse = (house) => {
+    this.setState({ activeHouse: house});
+  }
+
+  render() {
+    return(
+    <AppPresentation country={this.state.country}
+    filteredHouses={this.state.filteredHouses}
+    featuredHouse={this.state.featuredHouse}
+    countries={this.state.countries} filterHouses={this.filterHouses}
+    activeHouse={this.state.activeHouse} setActiveHouse={this.setActiveHouse}
+/>
+    )
+  }
+}
+
+export default App;
